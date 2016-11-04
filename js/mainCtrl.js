4ac90da4e92fe3a35ec6c966e12425b7b572b38f
jacoblemperle <jacob.lemperle@gmail.com>
2016-11-04 09:45:44 -0600

    added pages

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -11,6 +11,7 @@ angular.module('statsApp').controller('mainCtrl', function($scope, mainService) 
     $scope.getSpecificChamp = function(id) {
         mainService.getSpecificChamp(id).then(function(champInfo) {
             $scope.realMVP = champInfo;
+            $scope.champName = id;
             console.log($scope.realMVP);
 
         });

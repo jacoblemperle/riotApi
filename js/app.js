de1900128422478e383127e334c656970c947fc5
jacoblemperle <jacob.lemperle@gmail.com>
2016-11-07 15:04:33 -0700

    deleted file

--- js/app.js
+++ js/app.js
@@ -3,15 +3,15 @@ angular.module('statsApp', ['ui.router'])
   $stateProvider
     .state('home',{
       url: '/',
-      templateUrl: "../views/home.html"
+      templateUrl: "views/home.html"
     })
     .state('about', {
       url: '/about',
-      templateUrl: "../views/about.html"
+      templateUrl: "views/about.html"
     })
     .state('contact', {
       url: '/contact',
-      templateUrl: "../views/contact.html"
+      templateUrl: "views/contact.html"
     });
 
     $urlRouterProvider
